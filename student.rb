ad7658e515b44bea1c066daf00d2a6f28d3744ac
christianonoh <onohchibyk@gmail.com>
2023-06-21 18:08:28 +0100

    commit teacher and student

---
+++ student.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+    super(id, age, parent_permission, name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
+end
