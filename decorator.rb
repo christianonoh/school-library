013d4a60f9ed0c7ee969d1b5140384192f4e7118
christianonoh <onohchibyk@gmail.com>
2023-06-21 18:07:48 +0100

    Add CapitalizeDecorator and TrimmerDecorator

--- decorator.rb
+++ decorator.rb
@@ -10,3 +10,15 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.slice(0, 10)
+  end
+end
