2ecd934f398e70c2125aa5e03185c2c3fd0743c0
christianonoh <onohchibyk@gmail.com>
2023-06-22 11:35:07 +0100

    Create the has-many/belongs-to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, parent_permission, name)
     @classroom = classroom
+    classroom&.add_student(self)
   end
 
   def play_hooky
